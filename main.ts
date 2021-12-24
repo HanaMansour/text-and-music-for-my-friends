8acc5bbaa758e972493247ecc21bc611f65ab861
HanaMansour <96612907+HanaMansour@users.noreply.github.com>
2021-12-24 09:29:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ahoj Zlato, chci ti rict, ze te mam rada!")
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
